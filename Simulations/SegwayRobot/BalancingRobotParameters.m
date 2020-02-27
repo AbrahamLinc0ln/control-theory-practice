a077eb8611588ed32f793fed8b45cbf38665996f
AbrahamLinc0ln <33333588+AbrahamLinc0ln@users.noreply.github.com>
2020-02-26 19:55:50 -0500

    Add files via upload

---
+++ Simulations/SegwayRobot/BalancingRobotParameters.m
@@ -0,0 +1,13 @@
+% Parameters for balancing robot
+% MT
+% units in cm
+
+% Assuming robot only moves in x
+BodyXDim = 5;
+BodyYDim = 60;
+BodyZDim = 20;
+
+MotorTorque = 14; %Nm
+
+WheelRadius = 5;
+WheelDepth = 2;
