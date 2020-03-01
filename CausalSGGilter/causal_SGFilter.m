c407db3ac9d501d51c7feeef46f863cc6f440f17
AbrahamLinc0ln <33333588+AbrahamLinc0ln@users.noreply.github.com>
2020-03-01 13:26:26 -0500

    Add files via upload

---
+++ CausalSGGilter/causal_SGFilter.m
@@ -0,0 +1,19 @@
+%% Causal 2nd Order Savitzky Golay FIR Filter Coefficients Generator
+% Marshall Trout
+% 2019
+
+function [A, filt] = causal_SGFilter(FrameLength, ts)
+% FrameLength = length of filter
+% ts = sample time
+% filter(3,:) = FIR coeffs for derivative of filtered signal
+% filter(2,:) = FIR coeffs for filtered signal
+% The filter rows can be used directly in a Simulink FIR block
+
+for i = (FrameLength-1):-1:0
+    A(1,i+1) = (ts*i^2);
+    A(2,i+1) = -1*i*ts;
+end
+A(3,:) = ones(FrameLength,1);
+A = A';
+
+filt = inv(A'*A)*A';
